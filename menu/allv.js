eb944a05fcbbbebbf886332365f8760e04e19bab
Zeydan1 <zeydanganz@gmail.com>
2021-11-28 15:23:58 +0700

    VBOT

---
+++ menu/allv.js
@@ -0,0 +1,35 @@
+const allv = `
+│⿻ *.promote @tag
+║⿻ *.demote @tag
+│⿻ *.group tutup
+║⿻ *.group buka
+│⿻ *.owner
+║┗⊱
+┗━━━━━━━━━━━━━━━━━━⬣
+╔─═─═⊱ *「 MENU 」* ─═─═⬣
+│┏⊱
+║⿻ *.groupmenu*
+│⿻ *.ownermenu*
+║⿻ *.searchmenu*
+│⿻ *.convertmenu*
+║⿻ *.audiomenu*
+│⿻ *.animemenu*
+║⿻ *.makermenu*
+│⿻ *.pornmenu*
+║⿻ *.randommenu*
+│⿻ *.othermenu*
+║⿻ *.vidtawuran*
+│┗⊱
+┗━━━━━━━━━━━━━━━━━━⬣
+┏━━⬣  Thanks To 〽️
+┃ 🔥 RANZ
+┃ 🔥 PAK TZY
+┃ 🔥 Arifi Razaq OFFICIAL
+┃ 🔥 HANZ OFC
+┃ 🔥 THUNDER
+┃ 🔥 BOT INDO
+┃ 🔥 MHANKAKBAR
+┃ 🔥 Xenn Bot
+┗━━⬣  ⿻ VBotz🎭 ⿻️
+`
+exports.allv = allv
